e09b6d46ee2458fe0baed9926bb364c9d8e00e75
coulon-estelle <estelle.coulon@edu.univ-fcomte.fr>
2025-11-13 14:55:11 +0100

    ouverture/fermeture menu.

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,4 @@
+
 // On cible les éléments à modifier
 const toggle = document.querySelector(".menu-btn");
 const nav = document.querySelector(".menu");
@@ -5,6 +6,7 @@ const nav = document.querySelector(".menu");
 // Ajoute un écouteur d'événements à l'élément 'toggle' qui écoute les événements 'click'.
 toggle.addEventListener("click", () => {
   // Mise à jour des attributs ARIA pour accessibilité
-  nav.ariaHidden = "false";
-  toggle.ariaExpanded = "true";
+const isOpen = toggle.getAttribute("aria-expanded") === "true";
+  nav.setAttribute("aria-hidden", String(isOpen)); 
+  toggle.setAttribute("aria-expanded", String(!isOpen)); 
 });
